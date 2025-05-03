b37d320c141fd88db4c70bba490540700fc6a48c
Dagmawi-22 <dagtt316@gmal.com>
2025-05-03 12:51:30 +0300

    improve flashcard deck ui, apply social sharing, copy link to clip board

--- src/routes/flashcards/+page.svelte
+++ src/routes/flashcards/+page.svelte
@@ -1,21 +1,6 @@
 <script lang="ts">
-	import FlashcardDeck from '../../components/FlashcardDeck.svelte';
-	import { flashCardStore } from '../../stores/flashcard';
-	import { onMount } from 'svelte';
-	import { get } from 'svelte/store';
-	import { page } from '$app/stores';
-
-	let initialFlashcards: { front: string; back: string; id?: string }[] = [];
-
-	$: deckId = $page.params.deckId || 'user-flashcards';
-
-	onMount(() => {
-		const storeFlashcards = get(flashCardStore);
-		initialFlashcards = storeFlashcards.map((card, index) => ({
-			...card,
-			id: `user-card-${index}`
-		}));
-	});
+    import FlashcardDeck from '../../components/FlashcardDeck.svelte';
+    import { flashCardStore } from '../../stores/flashcard';
 </script>
 
-<FlashcardDeck {initialFlashcards} {deckId} />
+<FlashcardDeck initialFlashcards={$flashCardStore} />
